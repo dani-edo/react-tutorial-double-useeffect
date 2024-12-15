b11416e8adbaee221d752880c46ee228a8070afd
dani_edo <dani.edogawa.17@gmail.com>
2024-12-15 20:54:30 +0700

    double useEffect: clean up ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,77 @@
-function App() {
-  return <div style={{ fontFamily: "sans-serif" }}>React App</div>;
+import { useRef, useState } from "react";
+
+export default function AnimalFriends() {
+  const itemsRef = useRef([]);
+  const [animalList, setAnimalList] = useState(setupAnimalList);
+  const [animal, setAnimal] = useState("cat");
+
+  function scrollToAnimal(index) {
+    const list = itemsRef.current;
+    const { node } = list[index];
+    node.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  const animals = animalList.filter((a) => a.type === animal);
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => setAnimal("cat")}>Cats</button>
+        <button onClick={() => setAnimal("dog")}>Dogs</button>
+      </nav>
+      <hr />
+      <nav>
+        <span>Scroll to:</span>
+        {animals.map((animal, index) => (
+          <button key={animal.src} onClick={() => scrollToAnimal(index)}>
+            {index}
+          </button>
+        ))}
+      </nav>
+      <div>
+        <ul>
+          {animals.map((animal) => (
+            <li
+              key={animal.src}
+              ref={(node) => {
+                const list = itemsRef.current;
+                const item = { animal: animal, node };
+                list.push(item);
+                console.log(
+                  `✅ Adding animal to the map. Total animals: ${list.length}`
+                );
+                if (list.length > 10) {
+                  console.log("❌ Too many animals in the list!");
+                }
+              }}
+            >
+              <img src={animal.src} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default App;
+function setupAnimalList() {
+  const animalList = [];
+  for (let i = 0; i < 10; i++) {
+    animalList.push({
+      type: "cat",
+      src: "https://loremflickr.com/320/240/cat?lock=" + i,
+    });
+  }
+  for (let i = 0; i < 10; i++) {
+    animalList.push({
+      type: "dog",
+      src: "https://loremflickr.com/320/240/dog?lock=" + i,
+    });
+  }
+
+  return animalList;
+}
