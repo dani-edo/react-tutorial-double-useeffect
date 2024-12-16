9856c1e51919bc8de9fe28372a5538b17b375d1a
dani_edo <dani.edogawa.17@gmail.com>
2024-12-16 11:03:29 +0700

    double useEffect: deprecation warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,58 @@
-function App() {
-  return <div style={{ fontFamily: "sans-serif" }}>React App</div>;
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "Initial Data",
+    };
+    console.log("Constructor: Component is being initialized.");
+  }
+
+  UNSAFE_componentWillMount() {
+    console.log("UNSAFE_componentWillMount: Component is about to mount.");
+    // perform initialize state
+    // perform fetch data
+  }
+
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    console.log("UNSAFE_componentWillUpdate: Component is about to update.");
+    console.log("Next Props:", nextProps);
+    console.log("Next State:", nextState);
+    // perform updates based on prop changes
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    console.log("UNSAFE_componentWillReceiveProps: New props received.");
+    console.log("Next Props:", nextProps);
+    // state updates based on props
+    // side effects based on prop changes
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount: Component mounted successfully.");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate: Component updated successfully.");
+    console.log("Previous Props:", prevProps);
+    console.log("Previous State:", prevState);
+  }
+
+  updateData = () => {
+    this.setState({ data: "Updated Data" });
+  };
+
+  render() {
+    console.log("Render: Component is rendering.");
+    return (
+      <div>
+        <h1>React App</h1>
+        <p>Data: {this.state.data}</p>
+        <button onClick={this.updateData}>Update Data</button>
+      </div>
+    );
+  }
 }
 
 export default App;
